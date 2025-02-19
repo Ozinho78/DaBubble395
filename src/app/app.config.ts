e6eb840f14c11f206ee44088800484cec704bd90
PatrickKanert <patrickkanert@gmail.com>
2025-02-19 19:07:43 +0100

    app.config.ts customized

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { routes } from "./app.routes";
 
-import env from '../environments/env.json';
+import env from "../environments/env.json";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: env.FIREBASE_PROJECT_ID,
-        appId: env.FIREBASE_APP_ID,
-        storageBucket: env.FIREBASE_STORAGE_BUCKET,
-        apiKey: env.FIREBASE_API_KEY,
-        authDomain: env.FIREBASE_AUTH_DOMAIN,
-        messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
-      })
-    ),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-  ],
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+		provideFirebaseApp(() =>
+			initializeApp({
+				projectId: env.FIREBASE_PROJECT_ID,
+				appId: env.FIREBASE_APP_ID,
+				storageBucket: env.FIREBASE_STORAGE_BUCKET,
+				apiKey: env.FIREBASE_API_KEY,
+				authDomain: env.FIREBASE_AUTH_DOMAIN,
+				messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+			}),
+		),
+		provideAuth(() => getAuth()),
+		provideFirestore(() => getFirestore()),
+	],
 };
