c4e5862f8b6d98370c5c609d260b252bd3d2f424
M1roel <ppfautsch@yahoo.de>
2025-02-20 19:52:01 +0100

    yes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent {
   constructor(private router: Router) {}
 
   login() {
-    localStorage.setItem('token', 'dummy-token');
-    this.router.navigate(['/main']);
+    localStorage.setItem('token', 'dummy-token'); // Simuliertes Login
+    this.router.navigate(['/main']); // Weiterleitung nach Login
   }
 }
