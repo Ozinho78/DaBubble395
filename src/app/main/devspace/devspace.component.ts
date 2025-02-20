4a6a8bf766b2f9bc8c4eef91cb83ca279c1bf3a4
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-02-20 22:05:07 +0100

    devspace first firestore test with onSnapshot

--- src/app/main/devspace/devspace.component.ts
+++ src/app/main/devspace/devspace.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-devspace',
@@ -7,5 +13,32 @@ import { Component } from '@angular/core';
   styleUrl: './devspace.component.scss'
 })
 export class DevspaceComponent {
+  firestore: Firestore = inject(Firestore);
+  
+  userDatabase = collection(this.firestore, 'users');
+  users: any = [{}];
+
+  unsubUserNames;
+
+  constructor(){
+    let i = 0;
+    this.unsubUserNames = onSnapshot(this.userDatabase, (list) => {
+      list.forEach(element => {
+        // this.users[element.id] = element.data();
+        this.users.push(element.data());
+        // console.log(element.data());
+        console.log(element.data());
+        
+      });
+    });
+  }
+
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.unsubUserNames();
+  }
+
 
 }
