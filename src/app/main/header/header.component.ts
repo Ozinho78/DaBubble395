ced725e031b337be255e79fcea1ef67b4f719d6e
PatrickKanert <patrickkanert@gmail.com>
2025-02-20 14:12:54 +0100

    feat: fetch and display user name from Firestore in header component

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +11,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   menuOpen: boolean = false;
+  userName!: Observable<any[]>;
+
+  constructor(private firestore: Firestore) {}
+
+  ngOnInit(): void {
+    // Ersetze 'deineCollectionName' mit dem tatsächlichen Namen deiner Collection in Firestore
+    const itemsRef = collection(this.firestore, 'users');
+    this.userName = collectionData(itemsRef);
+  }
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
