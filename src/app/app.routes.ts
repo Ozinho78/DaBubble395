a75f48edf0318d28cd97f4366a6a5f7a7e99253c
M1roel <ppfautsch@yahoo.de>
2025-02-20 21:29:32 +0100

    feat: add authentication components for registration, login, password reset, and avatar selection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main/main.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from '../app/auth/login/login.component';
 import { AuthGuard } from '../app/guards/auth.guard';
 
 export const routes: Routes = [
