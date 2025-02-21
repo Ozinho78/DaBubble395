866e16cfac15b297090aba3c3dcda6e679f4a050
M1roel <ppfautsch@yahoo.de>
2025-02-21 15:13:37 +0100

    feat: implement avatar selection component with styling and navigation

--- src/app/auth/avatar-selection/avatar-selection.component.ts
+++ src/app/auth/avatar-selection/avatar-selection.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-avatar-selection',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './avatar-selection.component.html',
   styleUrl: './avatar-selection.component.scss'
 })
 export class AvatarSelectionComponent {
 
+  avatars = [
+    { name: 'avatar1', src: '/img/avatar/avatar1.png' },
+    { name: 'avatar2', src: '/img/avatar/avatar2.png' },
+    { name: 'avatar3', src: '/img/avatar/avatar3.png' },
+    { name: 'avatar4', src: '/img/avatar/avatar4.png' },
+    { name: 'avatar5', src: '/img/avatar/avatar5.png' },
+    { name: 'avatar6', src: '/img/avatar/avatar6.png' }
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  navigateToReg() {
+    this.router.navigate(['/register']);
+  }
+
+  selectAvatar(avatar: any) {
+    console.log(avatar);
+  }
+
+  accept() {
+    console.log('accept');
+  }
 }
