05edd63a96d96c2fabeda350ded6ee2076d86af4
PatrickKanert <patrickkanert@gmail.com>
2025-02-25 12:28:44 +0100

    Enhance header component with profile editing features and improved UI elements

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,7 +1,9 @@
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Auth, signOut } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +13,15 @@ import { Observable } from 'rxjs';
 })
 export class HeaderComponent {
   menuOpen: boolean = false;
+  profileEditOpen: boolean = false;
   userName!: Observable<any[]>;
+  closeImgSrc: string = '/img/header-img/close.png';
 
-  constructor(private firestore: Firestore) {}
+  constructor(
+    private firestore: Firestore,
+    private auth: Auth,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const itemsRef = collection(this.firestore, 'users');
@@ -22,5 +30,33 @@ export class HeaderComponent {
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
+  }
+
+  logout(): void {
+    signOut(this.auth)
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        console.error('Logout-Fehler:', error);
+      });
+  }
+
+  toggleProfileEdit(): void {
+    this.profileEditOpen = !this.profileEditOpen;
+  }
+
+  close(): void {
+    this.menuOpen = false;
+    this.profileEditOpen = false;
+  }
+
+  onMouseEnterClose(): void {
+    this.closeImgSrc = '/img/header-img/close-hover.png';
+  }
+
+  // Wenn die Maus das Bild verlässt: zurück zum Standardbild
+  onMouseLeaveClose(): void {
+    this.closeImgSrc = '/img/header-img/close.png';
   }
 }
