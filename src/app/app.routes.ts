d27d38c48cb85e8c06159ab4c01c1d2d67152da9
M1roel <ppfautsch@yahoo.de>
2025-02-27 15:19:59 +0000

    Add set new password route and legal links to password reset component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from '../app/auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AvatarSelectionComponent } from './auth/avatar-selection/avatar-selection.component';
 import { RequestPasswordResetComponent } from './auth/request-password-reset/request-password-reset.component';
+import { SetNewPasswordComponent } from './auth/set-new-password/set-new-password.component';
 import { AuthGuard } from '../app/guards/auth.guard';
 import { RegisterGuard } from '../app/guards/register.guard';
 import { ImprintComponent } from './legal/imprint/imprint.component';
@@ -17,6 +18,7 @@ export const routes: Routes = [
   { path: 'avatar-selection', component: AvatarSelectionComponent, canActivate: [RegisterGuard, AuthGuard] },
   { path: 'main', component: MainComponent, canActivate: [AuthGuard] },
   { path: "request-password-reset", component: RequestPasswordResetComponent },
+  { path: "set-new-password", component: SetNewPasswordComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'policy', component: PolicyComponent },
 ];
