89dfa3214b22f7ef6d118d0c187d9f5a1e5439ba
PatrickKanert <patrickkanert@gmail.com>
2025-03-05 10:19:55 +0100

    refactor user service: improve code formatting, enhance user retrieval methods, and add email field to user data

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,95 +1,115 @@
-import { inject, Injectable, Injector, runInInjectionContext } from '@angular/core';
-import { Firestore, getDocs, collection, collectionData, doc, addDoc, docData, query, where } from '@angular/fire/firestore';
+import {
+  inject,
+  Injectable,
+  Injector,
+  runInInjectionContext,
+} from '@angular/core';
+import {
+  Firestore,
+  getDocs,
+  collection,
+  collectionData,
+  doc,
+  addDoc,
+  docData,
+  query,
+  where,
+} from '@angular/fire/firestore';
 import { Observable, firstValueFrom, map } from 'rxjs';
 import { AuthService } from './auth.service';
 import { User } from '../models/user.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-    private firestore = inject(Firestore);
-    private injector = inject(Injector);
-    private authService = inject(AuthService);
+  private firestore = inject(Firestore);
+  private injector = inject(Injector);
+  private authService = inject(AuthService);
 
-    user = new User();
-    userData = this.authService.userData;
+  user = new User();
+  userData = this.authService.userData;
 
-    /** Holt einen einzelnen Nutzer anhand der ID */
-    getUserById(userId: string): Observable<{ name: string, avatar: string }> {
-        const userRef = doc(this.firestore, `users/${userId}`);
+  /** Holt einen einzelnen Nutzer anhand der ID */
+  getUserById(
+    userId: string
+  ): Observable<{ name: string; avatar: string; email: string }> {
+    const userRef = doc(this.firestore, `users/${userId}`);
 
-        return runInInjectionContext(this.injector, () =>
-            docData(userRef) as Observable<any>
-        ).pipe(
-            map((user: any) => ({
-                name: user?.name || 'Unbekannt',
-                avatar: user?.avatar ? `img/avatar/${user.avatar}` : 'img/avatar/default.png'
-            }))
-        );
+    return docData(userRef).pipe(
+      map((user: any) => ({
+        name: user?.name || 'Unbekannt',
+        avatar: user?.avatar
+          ? `img/avatar/${user.avatar}`
+          : 'img/avatar/default.png',
+        email: user?.email || '',
+      }))
+    );
+  }
+
+  getUserByEmail(email: string): Observable<any> {
+    const usersRef = collection(this.firestore, 'users'); // Firestore Users Collection
+    const queryRef = query(usersRef, where('email', '==', email));
+    return collectionData(queryRef, { idField: 'uid' }).pipe(
+      map((users) => (users.length > 0 ? users[0] : null))
+    );
+  }
+
+  async createUser(): Promise<void> {
+    if (!this.authService.hasUserData()) {
+      console.error('Keine Benutzerinformationen gefunden!');
+      return;
     }
 
-    getUserByEmail(email: string): Observable<any> {
-        const usersRef = collection(this.firestore, 'users'); // Firestore Users Collection
-        const queryRef = query(usersRef, where('email', '==', email));
-        return collectionData(queryRef, { idField: 'uid' }).pipe(
-            map(users => users.length > 0 ? users[0] : null)
-        );
+    const { name, email, avatar } = this.authService.userData;
+
+    const usersCollectionRef = collection(this.firestore, 'users');
+    await addDoc(usersCollectionRef, {
+      name,
+      email,
+      avatar: avatar || 'default.png',
+    });
+
+    console.log('Benutzer erfolgreich in Firestore gespeichert!');
+  }
+
+  /** Holt alle Nutzer aus der Firestore `users`-Sammlung */
+  getAllUsers(): Observable<{ name: string; avatar: string; id: string }[]> {
+    const usersRef = collection(this.firestore, 'users');
+
+    return runInInjectionContext(
+      this.injector,
+      () => collectionData(usersRef, { idField: 'id' }) as Observable<any[]>
+    ).pipe(
+      map((users) =>
+        users.map((user) => ({
+          id: user.id,
+          name: user?.name || 'Unbekannt',
+          avatar: user?.avatar
+            ? `img/avatar/${user.avatar}`
+            : 'img/avatar/default.png',
+        }))
+      )
+    );
+  }
+
+  async saveUserDocIdByEmail(email: string): Promise<void> {
+    try {
+      const usersCollection = collection(this.firestore, 'users');
+
+      // Query: Suche nach dem User mit der passenden Email
+      const q = query(usersCollection, where('email', '==', email));
+      const snapshot = await getDocs(q);
+
+      if (!snapshot.empty) {
+        const userDoc = snapshot.docs[0];
+        const docId = userDoc.id;
+        localStorage.setItem('user-id', docId);
+      } else {
+        console.error('Kein Benutzer mit dieser E-Mail gefunden.');
+      }
+    } catch (error) {
+      console.error('Fehler beim Abrufen des Users:', error);
     }
-
-    async createUser(): Promise<void> {
-        if (!this.authService.hasUserData()) {
-            console.error("Keine Benutzerinformationen gefunden!");
-            return;
-        }
-
-        const { name, email, avatar } = this.authService.userData;
-
-        const usersCollectionRef = collection(this.firestore, 'users');
-        await addDoc(usersCollectionRef, {
-            name,
-            email,
-            avatar: avatar || 'default.png'
-        });
-
-        console.log('Benutzer erfolgreich in Firestore gespeichert!');
-    }
-
-
-    /** Holt alle Nutzer aus der Firestore `users`-Sammlung */
-    getAllUsers(): Observable<{ name: string, avatar: string, id: string }[]> {
-        const usersRef = collection(this.firestore, 'users');
-
-        return runInInjectionContext(this.injector, () =>
-            collectionData(usersRef, { idField: 'id' }) as Observable<any[]>
-        ).pipe(
-            map(users =>
-                users.map(user => ({
-                    id: user.id,
-                    name: user?.name || 'Unbekannt',
-                    avatar: user?.avatar ? `img/avatar/${user.avatar}` : 'img/avatar/default.png'
-                }))
-            )
-        );
-    }
-
-    async saveUserDocIdByEmail(email: string): Promise<void> {
-        try {
-            const usersCollection = collection(this.firestore, 'users');
-
-            // Query: Suche nach dem User mit der passenden Email
-            const q = query(usersCollection, where('email', '==', email));
-            const snapshot = await getDocs(q);
-
-            if (!snapshot.empty) {
-                const userDoc = snapshot.docs[0];
-                const docId = userDoc.id;
-                localStorage.setItem('user-id', docId);
-            } else {
-                console.error('Kein Benutzer mit dieser E-Mail gefunden.');
-            }
-        } catch (error) {
-            console.error('Fehler beim Abrufen des Users:', error);
-        }
-    }
+  }
 }
