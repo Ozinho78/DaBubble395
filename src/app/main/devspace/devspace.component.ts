01fc3075a99fdf03631f8acd5796529f1e25d116
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-03-07 21:54:16 +0100

    devspace changed to async loading of arrays

--- src/app/main/devspace/devspace.component.ts
+++ src/app/main/devspace/devspace.component.ts
@@ -6,9 +6,10 @@ import { Channel } from '../../../models/channel.model';
 import { AddChannelComponent } from './add-channel/add-channel.component';
 import { CommonModule } from '@angular/common';
 import { VisibleService } from '../../../services/visible.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { UserService } from '../../../services/user.service';
+import { FirestoreService } from '../../../services/firestore.service';
 
 @Injectable({
   providedIn: 'root',
@@ -32,6 +33,8 @@ import { UserService } from '../../../services/user.service';
   ]
 })
 export class DevspaceComponent implements OnInit {
+  users$!: Observable<User[]>;
+  channels$!: Observable<Channel[]>;
   isVisible = true;
   isChannelVisible = true;
   isUserVisible = true;
@@ -47,81 +50,12 @@ export class DevspaceComponent implements OnInit {
   docId: string | null = null;
   channelId: string | null = null;
 
-
-  toggleChannelVisibility() {
-    this.isChannelVisible = !this.isChannelVisible;
-  }
-
-  toggleUserVisibility(){
-   this.isUserVisible = !this.isUserVisible;
-  }
-  
-
-  testData = [
-    {
-      password: '6Boh2tS3saxHDWswwuGX',
-      avatar: 'avatar1.png',
-      name: 'Steffen Hoffmann',
-      mail: 'steffen.hoffmann@hotmail.com',
-    },
-    {
-      avatar: 'avatar3.png',
-      password: 'Tng55pMIXGbUfhOqkU63',
-      mail: 'elise.roth@da-akademie.com',
-      name: 'Elise Roth',
-    },
-    {
-      password: '4081752b278a47a544ed7afc0d738b02',
-      mail: 'sofia.mueller@offline.gov',
-      name: 'Sofia Müller',
-      avatar: 'avatar5.png',
-    },
-    {
-      name: 'Noah Braun',
-      mail: 'noah.braun@example.com',
-      avatar: 'avatar4.png',
-      password: '5f4dcc3b5aa765d61d8327deb882cf99',
-    },
-    {
-      name: 'Frederik Beck',
-      mail: 'fred.beck@email.com',
-      password: 'c581a959b6972b909d6e4cec06670c08',
-      avatar: 'avatar6.png',
-    },
-    {
-      mail: 'elias.neumann@gmail.com',
-      avatar: 'avatar2.png',
-      password: 'zW8U5VZ5wbrb8Ng8RfKE',
-      name: 'Elias Neumann',
-    },
-  ];
-
   @ViewChild(AddChannelComponent) channelDialog!: AddChannelComponent;
 
-  unsubUserNames;
-  unsubChannelNames;
+  // unsubUserNames;
+  // unsubChannelNames;
 
-  constructor(private visibleService: VisibleService, private userService: UserService) {
-    this.unsubUserNames = onSnapshot(this.userDatabase, (list) => {
-      this.users = [];
-      list.forEach((element) => {
-        // this.users[element.id] = element.data();
-        let singleUser = element.data() as User;
-        let str = singleUser.avatar;
-        singleUser.id = parseInt(str.match(/\d+/)?.[0] || '0', 10);
-        singleUser.docId = element.id;
-        this.users.push(singleUser);
-        // console.log(element.data());
-      });
-    });
-    this.unsubChannelNames = onSnapshot(this.channelDatabase, (list) => {
-      this.channels = [];
-      list.forEach((element) => {
-        const singleChannel = element.data() as Channel;
-        singleChannel.docId = element.id;
-        this.channels.push(singleChannel);
-      });
-    });
+  constructor(private dataService: FirestoreService, private visibleService: VisibleService, private userService: UserService){
     setTimeout(() => {
       this.userLoggedIn = localStorage.getItem('user-id') || '';
     }, 500);
@@ -132,6 +66,10 @@ export class DevspaceComponent implements OnInit {
     // this.subscription = this.visibleService.visibleState$.subscribe(value => {
     //   this.isVisible = value;
     // });
+    this.users$ = this.dataService.users$;
+    this.channels$ = this.dataService.channels$;
+    this.users = this.dataService.getUsers();
+    this.channels = this.dataService.getChannels();
     this.visibleService.visibleState$.subscribe(value => {
       this.isVisible = value;
     });
@@ -148,6 +86,14 @@ export class DevspaceComponent implements OnInit {
     // );
   }
 
+  toggleChannelVisibility() {
+    this.isChannelVisible = !this.isChannelVisible;
+  }
+
+  toggleUserVisibility(){
+   this.isUserVisible = !this.isUserVisible;
+  }
+
   sortUsersByAvatar() {
     this.users.sort((a, b) => {
       const start = parseInt(a.avatar.match(/\d+/)?.[0] || '0', 10);
@@ -159,8 +105,8 @@ export class DevspaceComponent implements OnInit {
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
-    this.unsubUserNames();
-    this.unsubChannelNames();
+    // this.unsubUserNames();
+    // this.unsubChannelNames();
     this.userLoggedIn = '';
   }
 
@@ -205,3 +151,75 @@ export class DevspaceComponent implements OnInit {
   }
 
 }
+
+
+
+
+// alter Constructor ohne FirestoreService
+// constructor(private dataService: FirestoreService, private visibleService: VisibleService, private userService: UserService) {
+//   this.unsubUserNames = onSnapshot(this.userDatabase, (list) => {
+//     this.users = [];
+//     list.forEach((element) => {
+//       // this.users[element.id] = element.data();
+//       let singleUser = element.data() as User;
+//       let str = singleUser.avatar;
+//       singleUser.id = parseInt(str.match(/\d+/)?.[0] || '0', 10);
+//       singleUser.docId = element.id;
+//       this.users.push(singleUser);
+//       // console.log(element.data());
+//     });
+//   });
+//   this.unsubChannelNames = onSnapshot(this.channelDatabase, (list) => {
+//     this.channels = [];
+//     list.forEach((element) => {
+//       const singleChannel = element.data() as Channel;
+//       singleChannel.docId = element.id;
+//       this.channels.push(singleChannel);
+//     });
+//   });
+//   setTimeout(() => {
+//     this.userLoggedIn = localStorage.getItem('user-id') || '';
+//   }, 500);
+// }
+
+
+
+// testData = [
+//   {
+//     password: '6Boh2tS3saxHDWswwuGX',
+//     avatar: 'avatar1.png',
+//     name: 'Steffen Hoffmann',
+//     mail: 'steffen.hoffmann@hotmail.com',
+//   },
+//   {
+//     avatar: 'avatar3.png',
+//     password: 'Tng55pMIXGbUfhOqkU63',
+//     mail: 'elise.roth@da-akademie.com',
+//     name: 'Elise Roth',
+//   },
+//   {
+//     password: '4081752b278a47a544ed7afc0d738b02',
+//     mail: 'sofia.mueller@offline.gov',
+//     name: 'Sofia Müller',
+//     avatar: 'avatar5.png',
+//   },
+//   {
+//     name: 'Noah Braun',
+//     mail: 'noah.braun@example.com',
+//     avatar: 'avatar4.png',
+//     password: '5f4dcc3b5aa765d61d8327deb882cf99',
+//   },
+//   {
+//     name: 'Frederik Beck',
+//     mail: 'fred.beck@email.com',
+//     password: 'c581a959b6972b909d6e4cec06670c08',
+//     avatar: 'avatar6.png',
+//   },
+//   {
+//     mail: 'elias.neumann@gmail.com',
+//     avatar: 'avatar2.png',
+//     password: 'zW8U5VZ5wbrb8Ng8RfKE',
+//     name: 'Elias Neumann',
+//   },
+// ];
+
