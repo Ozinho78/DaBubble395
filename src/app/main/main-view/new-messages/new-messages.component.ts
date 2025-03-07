329880990a62155a87aa28073e3a761968f14b2e
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-03-07 21:31:55 +0100

    firestore service added

--- src/app/main/main-view/new-messages/new-messages.component.ts
+++ src/app/main/main-view/new-messages/new-messages.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
-import { NewMessagesInputComponent } from "./new-messages-input/new-messages-input.component";
+import { Component, inject, OnInit } from '@angular/core';
+import { User } from '../../../../models/user.model';
+import { Channel } from '../../../../models/channel.model';
+import { Observable } from 'rxjs';
+import { FirestoreService } from '../../../../services/firestore.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-new-messages',
-  imports: [NewMessagesInputComponent],
+  imports: [CommonModule],
   templateUrl: './new-messages.component.html',
   styleUrl: './new-messages.component.scss'
 })
-export class NewMessagesComponent {
+export class NewMessagesComponent implements OnInit {
+  users$!: Observable<User[]>;
+  channels$!: Observable<Channel[]>;
+  
+  users: User[] = [];
+  channels: Channel[] = [];
 
+  constructor(private dataService: FirestoreService) {}
 
+  ngOnInit() {
+    this.users$ = this.dataService.users$;
+    this.channels$ = this.dataService.channels$;
 
+    // Direkter Zugriff auf die Arrays
+    this.users = this.dataService.getUsers();
+    this.channels = this.dataService.getChannels();
+  }
 }
 
