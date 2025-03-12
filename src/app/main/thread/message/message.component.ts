bd3f3b6678b69fd2e03246a4c02754c0de777194
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-03-12 18:08:45 +0100

    feat(thread): update message component to use currentUserId for own message styling and add edit menu

--- src/app/main/thread/message/message.component.ts
+++ src/app/main/thread/message/message.component.ts
@@ -1,13 +1,11 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-//import { Firestore } from '@angular/fire/firestore';
 import { ReactionsComponent } from '../../reactions/reactions.component';
-import { Message } from '../../../../models/message.class';
-import { Reaction } from '../../../../models/reaction.class';
-import { UserService } from '../../../../services/user.service';
 import { ReactionService } from '../../../../services/reaction.service';
-//import { AuthService } from '../../../../services/auth.service';
+import { Reaction } from '../../../../models/reaction.class';
+import { Message } from '../../../../models/message.class';
+import { UserService } from '../../../../services/user.service';
 
 @Component({
   selector: 'app-message',
@@ -15,18 +13,13 @@ import { ReactionService } from '../../../../services/reaction.service';
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss']
 })
-export class MessageComponent implements OnInit {
+export class MessageComponent implements OnInit, OnChanges {
   @Input() message!: Message;
   @Output() editRequest = new EventEmitter<{ id: string, text: string }>();
 
-  channelId: string | null = null;
-  threadId: string | null = null;
-
   currentUserId: string | null = null;
-  //currentUserData: any | null = null;
   currentUser: any;
   userData$!: Observable<{ name: string, avatar: string }>;
-  userCache: Map<string, Observable<{ name: string, avatar: string }>> = new Map();
   userNamesCache: { [userId: string]: string } = {};
   reactions$!: Observable<Reaction[]>;
   groupedReactions: { [type: string]: { count: number, likedByMe: boolean, userNames: string[] } } = {};
@@ -34,90 +27,92 @@ export class MessageComponent implements OnInit {
   showReactionTooltip: boolean = false;
   tooltipEmoji: string = '';
   tooltipText: string = '';
-  menuOpen: boolean = false; // Menü-Zustand
+  menuOpen: boolean = false;
 
   constructor(
-    //private firestore: Firestore,
     private userService: UserService,
     private reactionService: ReactionService,
-    //private authService: AuthService
   ) { }
 
-  ngOnInit(): void {
-    this.currentUserId = this.userService.getCurrentUserId();
-    this.loadCurrentName();
-    this.userData$ = this.userService.getUserById(this.message.userId);
+  ngOnInit() {
+    this.initializeUser();
+    this.subscribeToThreadChanges();
     this.loadReactions();
   }
 
-  loadReactions() {
-    // Lade die Reaktionen
-    if (this.message.id) {
-      this.reactions$ = this.reactionService.getReactions('messages', this.message.id);
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['message'] && changes['message'].currentValue) {
+      this.loadUserData();
+    }
+  }
 
-      this.reactions$.subscribe(reactions => {
-        const groups = reactions.reduce((acc, reaction) => {
-          if (!acc[reaction.type]) {
-            acc[reaction.type] = { count: 0, likedByMe: false, userNames: [] };
-          }
-          acc[reaction.type].count++;
+  loadUserData() {
+    if (this.message?.userId) {
+      this.userData$ = this.userService.getUserById(this.message.userId);
+    }
+  }
 
-          // Aktualisiere nur die Gruppe für den aktuellen Reaction-Typ
-          if (!this.userNamesCache[reaction.userId]) {
-            this.userService.getUserById(reaction.userId).subscribe(userData => {
-              this.userNamesCache[reaction.userId] = userData.name;
-              if (acc[reaction.type].userNames.indexOf(userData.name) === -1) {
-                acc[reaction.type].userNames.push(userData.name);
-              }
-            });
-          } else {
-            const name = this.userNamesCache[reaction.userId];
-            if (acc[reaction.type].userNames.indexOf(name) === -1) {
-              acc[reaction.type].userNames.push(name);
-            }
-          }
-          if (reaction.userId === this.currentUserId) {
-            acc[reaction.type].likedByMe = true;
-          }
-          return acc;
-        }, {} as { [type: string]: { count: number, likedByMe: boolean, userNames: string[] } });
-        this.groupedReactions = groups;
+  initializeUser() {
+    this.currentUserId = this.userService.getCurrentUserId();
+    this.loadCurrentName();
+  }
+
+  subscribeToThreadChanges() {
+    if (!this.message) return;
+    this.userData$ = this.userService.getUserById(this.message.userId);
+  }
+
+  loadCurrentName() {
+    if (this.currentUserId) {
+      this.userService.loadCurrentUser(this.currentUserId).then(user => {
+        this.currentUser = user;
       });
     }
   }
 
-  /*
-  setUser() {
-    this.authService.user$.subscribe(user => {
-      if (user) {
-        this.matchAuthUser(user.email);
-      }
-    });
+  loadReactions() {
+    if (!this.message.id) return;
+
+    this.reactions$ = this.reactionService.getReactions('messages', this.message.id);
+
+    this.reactions$.subscribe(reactions => this.groupReactions(reactions));
   }
 
-  matchAuthUser(email: string) {
-    if (!email) return;
-
-    this.userService.getUserByEmail(email).subscribe(userData => {
-      if (userData) {
-        this.currentUserId = userData.uid;
-        this.currentUserData = userData;
-      } else {
-        console.log('Kein User in der Datenbank mit dieser E-Mail gefunden.');
+  groupReactions(reactions: Reaction[]) {
+    const groups = reactions.reduce((acc, reaction) => {
+      if (!acc[reaction.type]) {
+        acc[reaction.type] = { count: 0, likedByMe: false, userNames: [] };
       }
-    });
-  }
-  */
+      acc[reaction.type].count++;
 
-  /*
-  getUserData(userId: string): Observable<{ name: string, avatar: string }> {
-    if (!this.userCache.has(userId)) {
-      const userData$ = this.userService.getUserById(userId);
-      this.userCache.set(userId, userData$);
+      this.processReactionUser(acc, reaction);
+      if (reaction.userId === this.currentUserId) {
+        acc[reaction.type].likedByMe = true;
+      }
+      return acc;
+    }, {} as { [type: string]: { count: number, likedByMe: boolean, userNames: string[] } });
+
+    this.groupedReactions = groups;
+  }
+
+  processReactionUser(
+    acc: { [type: string]: { count: number, likedByMe: boolean, userNames: string[] } },
+    reaction: Reaction
+  ) {
+    if (!this.userNamesCache[reaction.userId]) {
+      this.userService.getUserById(reaction.userId).subscribe(userData => {
+        this.userNamesCache[reaction.userId] = userData.name;
+        if (!acc[reaction.type].userNames.includes(userData.name)) {
+          acc[reaction.type].userNames.push(userData.name);
+        }
+      });
+    } else {
+      const name = this.userNamesCache[reaction.userId];
+      if (!acc[reaction.type].userNames.includes(name)) {
+        acc[reaction.type].userNames.push(name);
+      }
     }
-    return this.userCache.get(userId)!;
   }
-  */
 
   toggleReactionsOverlay(): void {
     this.showReactionsOverlay = !this.showReactionsOverlay;
@@ -137,11 +132,11 @@ export class MessageComponent implements OnInit {
       .catch(error => console.error('Fehler beim Hinzufügen der Reaction:', error));
   }
 
-  onOverlayClosed(): void {
+  onOverlayClosed() {
     this.showReactionsOverlay = false;
   }
 
-  removeMyReaction(): void {
+  removeMyReaction() {
     this.reactionService.removeReaction('messages', this.message.id!, this.currentUserId!)
       .then(() => {
         console.log('Reaction entfernt!');
@@ -150,28 +145,17 @@ export class MessageComponent implements OnInit {
       .catch(error => console.error('Fehler beim Entfernen der Reaction:', error));
   }
 
-  /*groupAccHasName(
-    acc: { [type: string]: { count: number, likedByMe: boolean, userNames: string[] } },
-    reactionType: string,
-    name: string
-  ): boolean {
-    return acc[reactionType]?.userNames.includes(name);
-  }*/
-
   openReactionTooltip(emoji: string, userNames: string[]) {
     this.tooltipEmoji = emoji;
     const names = userNames.map(name => (name === this.currentUserName ? 'Du' : name));
 
     if (names.length === 1) {
-      // Fall: Nur eine Person hat reagiert
       this.tooltipText = names[0] === 'Du' ? 'Du hast reagiert' : `${names[0]} hat reagiert`;
     } else if (names.length === 2) {
-      // Fall: Zwei Personen haben reagiert
       this.tooltipText = `${names[0]} und ${names[1]} haben reagiert`;
     } else {
-      // Fall: Drei oder mehr Personen haben reagiert
-      const allButLast = names.slice(0, -1).join(', '); // Alle außer den letzten mit Komma trennen
-      const last = names[names.length - 1]; // Letzter Name
+      const allButLast = names.slice(0, -1).join(', ');
+      const last = names[names.length - 1];
       this.tooltipText = `${allButLast} und ${last} haben reagiert`;
     }
 
@@ -180,14 +164,6 @@ export class MessageComponent implements OnInit {
 
   closeReactionTooltip() {
     this.showReactionTooltip = false;
-  }
-
-  loadCurrentName() {
-    if (this.currentUserId) {
-      this.userService.loadCurrentUser(this.currentUserId).then(user => {
-        this.currentUser = user;
-      });
-    }
   }
 
   get currentUserName(): string {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,20 +1,20 @@
 import {
-  inject,
-  Injectable,
-  Injector,
-  runInInjectionContext,
+    inject,
+    Injectable,
+    Injector,
+    runInInjectionContext,
 } from '@angular/core';
 import {
-  Firestore,
-  getDocs,
-  collection,
-  collectionData,
-  doc,
-  addDoc,
-  docData,
-  query,
-  where,
-  getDoc,
+    Firestore,
+    getDocs,
+    collection,
+    collectionData,
+    doc,
+    addDoc,
+    docData,
+    query,
+    where,
+    getDoc,
 } from '@angular/fire/firestore';
 import { BehaviorSubject, Observable, firstValueFrom, map } from 'rxjs';
 import { AuthService } from './auth.service';
@@ -22,174 +22,182 @@ import { User } from '../models/user.model';
 import { FirestoreService } from './firestore.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class UserService {
-  private firestore = inject(Firestore);
-  private injector = inject(Injector);
-  private authService = inject(AuthService);
-  private docIdFromDevSpace = new BehaviorSubject<string | null>(null);
-  private channelIdFromDevSpace = new BehaviorSubject<string | null>(null);
-  currentDocIdFromDevSpace = this.docIdFromDevSpace.asObservable();
-  currentChannelIdFromDevSpace = this.channelIdFromDevSpace.asObservable();
+    private firestore = inject(Firestore);
+    private injector = inject(Injector);
+    private authService = inject(AuthService);
+    private docIdFromDevSpace = new BehaviorSubject<string | null>(null);
+    private channelIdFromDevSpace = new BehaviorSubject<string | null>(null);
+    currentDocIdFromDevSpace = this.docIdFromDevSpace.asObservable();
+    currentChannelIdFromDevSpace = this.channelIdFromDevSpace.asObservable();
 
-  private currentUser: any = null;
+    private currentUser: any = null;
 
-  user = new User();
-  userData = this.authService.userData;
+    user = new User();
+    userData = this.authService.userData;
 
-  private channelId: string | null = null;
-  private userArray: User[] = [];
+    private channelId: string | null = null;
+    private userArray: User[] = [];
 
-  constructor(private firestoreService: FirestoreService) {
-    this.loadUsers();
-  }
-
-  /** Holt einen einzelnen Nutzer anhand der ID */
-  getUserById(
-    userId: string
-  ): Observable<{ name: string; avatar: string; email: string }> {
-    const userRef = doc(this.firestore, `users/${userId}`);
-
-    return docData(userRef).pipe(
-      map((user: any) => ({
-        name: user?.name || 'Unbekannt',
-        avatar: user?.avatar
-          ? `img/avatar/${user.avatar}`
-          : 'img/avatar/default.png',
-        email: user?.email || '',
-      }))
-    );
-  }
-
-  getUserByEmail(email: string): Observable<any> {
-    const usersRef = collection(this.firestore, 'users'); // Firestore Users Collection
-    const queryRef = query(usersRef, where('email', '==', email));
-    return collectionData(queryRef, { idField: 'uid' }).pipe(
-      map((users) => (users.length > 0 ? users[0] : null))
-    );
-  }
-
-  async createUser(): Promise<void> {
-    if (!this.authService.hasUserData()) {
-      console.error('Keine Benutzerinformationen gefunden!');
-      return;
+    constructor(private firestoreService: FirestoreService) {
+        this.loadUsers();
     }
 
-    const { name, email, avatar } = this.authService.userData;
+    getUserById(userId: string): Observable<{ name: string; avatar: string; email: string }> {
+        const cachedUser = this.userArray.find(user => user.docId === userId);
 
-    const usersCollectionRef = collection(this.firestore, 'users');
-    await addDoc(usersCollectionRef, {
-      name,
-      email,
-      avatar: avatar || 'default.png',
-    });
-
-    console.log('Benutzer erfolgreich in Firestore gespeichert!');
-  }
-
-  /** Holt alle Nutzer aus der Firestore `users`-Sammlung */
-  getAllUsers(): Observable<{ name: string; avatar: string; id: string }[]> {
-    const usersRef = collection(this.firestore, 'users');
-
-    return runInInjectionContext(
-      this.injector,
-      () => collectionData(usersRef, { idField: 'id' }) as Observable<any[]>
-    ).pipe(
-      map((users) =>
-        users.map((user) => ({
-          id: user.id,
-          name: user?.name || 'Unbekannt',
-          avatar: user?.avatar
-            ? `img/avatar/${user.avatar}`
-            : 'img/avatar/default.png',
-        }))
-      )
-    );
-  }
-
-  async saveUserDocIdByEmail(email: string): Promise<void> {
-    try {
-      const usersCollection = collection(this.firestore, 'users');
-
-      // Query: Suche nach dem User mit der passenden Email
-      const q = query(usersCollection, where('email', '==', email));
-      const snapshot = await getDocs(q);
-
-      if (!snapshot.empty) {
-        const userDoc = snapshot.docs[0];
-        const docId = userDoc.id;
-        localStorage.setItem('user-id', docId);
-      } else {
-        console.error('Kein Benutzer mit dieser E-Mail gefunden.');
-      }
-    } catch (error) {
-      console.error('Fehler beim Abrufen des Users:', error);
+        if (cachedUser) {
+            return new Observable(observer => {
+                observer.next({
+                    name: cachedUser.name || 'Unbekannt',
+                    avatar: cachedUser.avatar ? `img/avatar/${cachedUser.avatar}` : 'img/avatar/default.png',
+                    email: cachedUser.email || '',
+                });
+                observer.complete();
+            });
+        }
+        const userRef = doc(this.firestore, `users/${userId}`);
+        return docData(userRef).pipe(
+            map((user: any) => {
+                return {
+                    name: user?.name || 'Unbekannt',
+                    avatar: user?.avatar ? `img/avatar/${user.avatar}` : 'img/avatar/default.png',
+                    email: user?.email || '',
+                };
+            })
+        );
     }
-  }
 
-  getCurrentUserId(): string | null {
-    return localStorage.getItem('user-id');
-  }
-
-  async loadCurrentUser(userId: string) {
-    if (this.currentUser) return this.currentUser;
-
-    const userDocRef = doc(this.firestore, 'users', userId);
-    const userSnap = await getDoc(userDocRef);
-
-    if (userSnap.exists()) {
-      this.currentUser = userSnap.data();
-      return this.currentUser;
-    } else {
-      console.error('User nicht gefunden.');
-      return null;
+    getUserByEmail(email: string): Observable<any> {
+        const usersRef = collection(this.firestore, 'users'); // Firestore Users Collection
+        const queryRef = query(usersRef, where('email', '==', email));
+        return collectionData(queryRef, { idField: 'uid' }).pipe(
+            map((users) => (users.length > 0 ? users[0] : null))
+        );
     }
-  }
 
-  // aktualisiert die gespeicherte docId aus DevSpace
-  // next(id) setzt einen neuen Wert, wodurch alle abonnierten Komponenten automatisch informiert werden
-  setDocIdFromDevSpace(id: string) {
-    this.docIdFromDevSpace.next(id);
-  }
+    async createUser(): Promise<void> {
+        if (!this.authService.hasUserData()) {
+            console.error('Keine Benutzerinformationen gefunden!');
+            return;
+        }
 
-  /*
-  setChannelIdFromDevSpace(id: string) {
-    this.channelIdFromDevSpace.next(id);
-  }*/
+        const { name, email, avatar } = this.authService.userData;
 
-  /**
-  * Lädt alle User aus Firestore und speichert sie lokal.
-  */
-  async loadUsers() {
-    try {
-      this.userArray = await this.firestoreService.getData<User>('users');
-    } catch (error) {
-      console.error('❌ Fehler beim Laden der User:', error);
+        const usersCollectionRef = collection(this.firestore, 'users');
+        await addDoc(usersCollectionRef, {
+            name,
+            email,
+            avatar: avatar || 'default.png',
+        });
+
+        console.log('Benutzer erfolgreich in Firestore gespeichert!');
     }
-  }
 
-  /**
-   * Gibt den Namen eines Users anhand der `userId` zurück.
-   * Falls der User nicht existiert, wird `"Unbekannter Benutzer"` zurückgegeben.
-   * @param userId Die Firestore-User-ID
-   * @returns Der Benutzername oder `"Unbekannter Benutzer"`
-   */
-  getUserNameById(userId: string) {
-    const userArray = this.userArray.find(u => u.docId === userId);
-    return userArray ? userArray.name : 'Unbekannter Benutzer';
-  }
+    /** Holt alle Nutzer aus der Firestore `users`-Sammlung */
+    getAllUsers(): Observable<{ name: string; avatar: string; id: string }[]> {
+        const usersRef = collection(this.firestore, 'users');
 
-  getUserAvatarById(userId: string) {
-    const userArray = this.userArray.find(u => u.docId === userId);
-    return userArray ? userArray.avatar : 'Unbekannter Avatar';
-  }
+        return runInInjectionContext(
+            this.injector,
+            () => collectionData(usersRef, { idField: 'id' }) as Observable<any[]>
+        ).pipe(
+            map((users) =>
+                users.map((user) => ({
+                    id: user.id,
+                    name: user?.name || 'Unbekannt',
+                    avatar: user?.avatar
+                        ? `img/avatar/${user.avatar}`
+                        : 'img/avatar/default.png',
+                }))
+            )
+        );
+    }
 
-  setChannelIdFromDevSpace(channelId: string) {
-    this.channelId = channelId;
-  }
+    async saveUserDocIdByEmail(email: string): Promise<void> {
+        try {
+            const usersCollection = collection(this.firestore, 'users');
 
-  getChannelIdFromDevSpace(): string | null {
-    return this.channelId;
-  }
+            // Query: Suche nach dem User mit der passenden Email
+            const q = query(usersCollection, where('email', '==', email));
+            const snapshot = await getDocs(q);
+
+            if (!snapshot.empty) {
+                const userDoc = snapshot.docs[0];
+                const docId = userDoc.id;
+                localStorage.setItem('user-id', docId);
+            } else {
+                console.error('Kein Benutzer mit dieser E-Mail gefunden.');
+            }
+        } catch (error) {
+            console.error('Fehler beim Abrufen des Users:', error);
+        }
+    }
+
+    getCurrentUserId(): string | null {
+        return localStorage.getItem('user-id');
+    }
+
+    async loadCurrentUser(userId: string) {
+        if (this.currentUser) return this.currentUser;
+
+        const userDocRef = doc(this.firestore, 'users', userId);
+        const userSnap = await getDoc(userDocRef);
+
+        if (userSnap.exists()) {
+            this.currentUser = userSnap.data();
+            return this.currentUser;
+        } else {
+            console.error('User nicht gefunden.');
+            return null;
+        }
+    }
+
+    // aktualisiert die gespeicherte docId aus DevSpace
+    // next(id) setzt einen neuen Wert, wodurch alle abonnierten Komponenten automatisch informiert werden
+    setDocIdFromDevSpace(id: string) {
+        this.docIdFromDevSpace.next(id);
+    }
+
+    /*
+    setChannelIdFromDevSpace(id: string) {
+      this.channelIdFromDevSpace.next(id);
+    }*/
+
+    /**
+    * Lädt alle User aus Firestore und speichert sie lokal.
+    */
+    async loadUsers() {
+        try {
+            this.userArray = await this.firestoreService.getData<User>('users');
+        } catch (error) {
+            console.error('❌ Fehler beim Laden der User:', error);
+        }
+    }
+
+    /**
+     * Gibt den Namen eines Users anhand der `userId` zurück.
+     * Falls der User nicht existiert, wird `"Unbekannter Benutzer"` zurückgegeben.
+     * @param userId Die Firestore-User-ID
+     * @returns Der Benutzername oder `"Unbekannter Benutzer"`
+     */
+    getUserNameById(userId: string) {
+        const userArray = this.userArray.find(u => u.docId === userId);
+        return userArray ? userArray.name : 'Unbekannter Benutzer';
+    }
+
+    getUserAvatarById(userId: string) {
+        const userArray = this.userArray.find(u => u.docId === userId);
+        return userArray ? userArray.avatar : 'Unbekannter Avatar';
+    }
+
+    setChannelIdFromDevSpace(channelId: string) {
+        this.channelId = channelId;
+    }
+
+    getChannelIdFromDevSpace(): string | null {
+        return this.channelId;
+    }
 }
