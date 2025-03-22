2cd68928ff100d2646efc618b9c2f73d60e55836
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-03-22 17:35:21 +0100

    create channelToChange from channelId

--- src/app/main/main-view/channel/show-channel/show-channel.component.ts
+++ src/app/main/main-view/channel/show-channel/show-channel.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { Channel } from '../../../../../models/channel.model';
+import { doc, Firestore, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-show-channel',
@@ -13,6 +14,36 @@ export class ShowChannelComponent {
 
   @Input() channelIdInput!: string;
   @Output() closeModal = new EventEmitter<void>();
+
+  channelToChange!: Channel | null;
+
+  constructor(private firestore: Firestore) {} // Firestore-Injektion
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['channelIdInput'] && this.channelIdInput) {
+      this.loadChannel(this.channelIdInput);
+    }
+  }
+
+  async loadChannel(docId: string) {
+    const docRef = doc(this.firestore, 'channels', docId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+      this.channelToChange = new Channel();
+      
+      this.channelToChange.docId = docSnap.id;
+      this.channelToChange.creationDate = data['creationDate'];
+      this.channelToChange.name = data['name'];
+      this.channelToChange.description = data['description'];
+      this.channelToChange.member = data['member'] || [];
+      this.channelToChange.userId = data['userId'];
+    } else {
+      console.log('Dokument nicht gefunden!');
+      this.channelToChange = null;
+    }
+  }
 
   
   openModal() {
