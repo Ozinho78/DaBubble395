596f5621906c3a7f817ccdc8147dd9a1ac94e028
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-03-22 17:50:33 +0100

    create loadUserToChange

--- src/app/main/main-view/channel/show-channel/show-channel.component.ts
+++ src/app/main/main-view/channel/show-channel/show-channel.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { Channel } from '../../../../../models/channel.model';
 import { doc, Firestore, getDoc } from '@angular/fire/firestore';
+import { User } from '../../../../../models/user.model';
 
 @Component({
   selector: 'app-show-channel',
@@ -16,8 +17,10 @@ export class ShowChannelComponent {
   @Output() closeModal = new EventEmitter<void>();
 
   channelToChange!: Channel | null;
+  userToChange!: User | null; // Neuer User, der anhand der userId geladen wird
 
-  constructor(private firestore: Firestore) {} // Firestore-Injektion
+  constructor(private firestore: Firestore) {
+  } // Firestore-Injektion
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['channelIdInput'] && this.channelIdInput) {
@@ -39,11 +42,39 @@ export class ShowChannelComponent {
       this.channelToChange.description = data['description'];
       this.channelToChange.member = data['member'] || [];
       this.channelToChange.userId = data['userId'];
+
+      // Lade den User basierend auf userId
+      if (this.channelToChange.userId) {
+        this.loadUser(this.channelToChange.userId);
+      }
     } else {
-      console.log('Dokument nicht gefunden!');
+      console.log('Kanal nicht gefunden!');
       this.channelToChange = null;
+      this.userToChange = null;
     }
   }
+
+  async loadUser(userId: string) {
+    const userRef = doc(this.firestore, 'users', userId);
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap.exists()) {
+      const data = userSnap.data();
+      this.userToChange = new User();
+
+      this.userToChange.docId = userSnap.id;
+      this.userToChange.id = data['id'];
+      this.userToChange.avatar = data['avatar'];
+      this.userToChange.name = data['name'];
+      this.userToChange.email = data['email'];
+      this.userToChange.password = data['password'];
+      this.userToChange.active = data['active'];
+    } else {
+      console.log('Benutzer nicht gefunden!');
+      this.userToChange = null;
+    }
+  }
+
 
   
   openModal() {
