9fa60a29a16f843fad49f4537c439dff7a29022f
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-04-11 20:50:03 +0200

    implement member modal with user list and styling

--- src/app/main/main-view/channel/member/member.component.ts
+++ src/app/main/main-view/channel/member/member.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-member',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './member.component.html',
     styleUrl: './member.component.scss'
 })
@@ -11,9 +12,17 @@ export class MemberComponent {
     @Input() channelIdInput!: string;
     @Output() closeModal = new EventEmitter<void>();
 
-    isOpen = false; // Steuert, ob das Modal sichtbar ist
+    isOpen = false;
+    showUserList: boolean = false;
 
     openModal() {
         this.isOpen = true;
+    }
+
+    close(event?: Event) {
+        if (!event || event.target === event.currentTarget) {
+            this.isOpen = false;
+            this.showUserList = false;
+        }
     }
 }
